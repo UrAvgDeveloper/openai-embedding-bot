718412b4d196f7ad9f104f14dd0ae14f5e187cfc
Aditya Rout <adi.b20612@gmail.com>
2023-03-17 14:28:28 +0530

    feat: gpt3.5 interaction

--- app.py
+++ app.py
@@ -11,18 +11,15 @@ import numpy as np
 COMPLETIONS_MODEL = "text-davinci-003"
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
-df=pd.read_csv('~/crawler/try1/processed/embeddings.csv', index_col=0)
+df=pd.read_csv(f"{os.path.dirname(os.path.abspath(__file__))}/processed/embeddings.csv", index_col=0)
 df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
 
 def answer_question(
     question,
     df=df,
-    model=COMPLETIONS_MODEL,
-    max_len=1800,
+    max_len=200,
     size="ada",
     debug=False,
-    max_tokens=500,
-    stop_sequence='\n\n###\n\n'
 ):
     """
     Answer a question based on the most similar context from the dataframe texts
@@ -30,8 +27,8 @@ def answer_question(
     context = create_context(
         question,
         df,
-        max_len=max_len,
-        size=size,
+        # max_len=max_len,
+        # size=size,
     )
     # If debug, print the raw model response
     if debug:
@@ -39,17 +36,19 @@ def answer_question(
         print("\n\n")
 
     try:
-        response = openai.Completion.create(
-            prompt=f"Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know\"\n\nContext: {context}\n\n---\n\nQuestion: {question}\nAnswer:",
-            temperature=0,
-            max_tokens=max_tokens,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-            stop=stop_sequence,
-            model=model,
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            temperature=0.5,
+            max_tokens=200,
+            messages=[
+                  {"role": "system", "content": f"You are a helpful assistant. You must strictly answer the question based on the context below, and if the question can't be answered based on the context or is not related to the context below, say \"This is not related to Fetch.ai\" and nothing else. Also share me the link that will give me more information.\n\nContext: {context}"},
+                  {"role": "user", "content": "Question: What is fetch.ai"},
+                  {"role": "assistant", "content": "Our mission is to build the infrastructure required for developing modern, decentralized and peer-to-peer (P2P) applications that are free from centralized rent-seeking."},
+                  {"role": "user", "content": f"Question: {question}"}
+              ]
         )
-        return response["choices"][0]["text"].strip()
+        print("resp", response)
+        return response["choices"][0]["message"]["content"]
     except Exception as e:
         print(e)
         return ""
@@ -73,7 +72,7 @@ def compute_doc_embeddings(df: pd.DataFrame):
     }
 
 def create_context(
-    question, df, max_len=1800, size="ada"
+    question, df, max_len=500, size="ada"
 ):
     """
     Create a context for a question by finding the most similar context from the dataframe
@@ -91,19 +90,18 @@ def create_context(
 
     # Sort by distance and add the text to the context until the context is too long
     for i, row in df.sort_values('distances', ascending=True).iterrows():
-
         # Add the length of the text to the current length
         cur_len += row['n_tokens'] + 4
 
         # If the context is too long, break
-        if cur_len > max_len:
+        if cur_len > max_len or row['distances'] > 0.2:
             break
 
         # Else add it to the text that is being returned
         returns.append(row["text"])
 
     # Return the context
-    return "\n\n###\n\n".join(returns)
+    return ". ".join(returns)
 
 
 app = Flask(__name__)
@@ -115,7 +113,7 @@ def index():
     if request.method == "POST":
         question = request.form["question"]
         print("question =>",question)
-        return redirect(url_for("index", result=answer_question(question, debug=False)))
+        return redirect(url_for("index", result=answer_question(question, debug=True)))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
