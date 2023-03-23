68385c06de53a27d206c3cd7a1ea8d08ba20a029
Aditya Rout <adi.b20612@gmail.com>
2023-03-23 16:23:58 +0530

    feat: add chat moderation

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
+import backoff
 import os
-
 import openai
 from flask import Flask, redirect, render_template, request, url_for
-
 from openai.embeddings_utils import distances_from_embeddings
-import openai
 import pandas as pd
 import numpy as np
 
 COMPLETIONS_MODEL = "text-davinci-003"
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
-df=pd.read_csv(f"{os.path.dirname(os.path.abspath(__file__))}/processed/embeddings.csv", index_col=0)
+df = pd.read_csv(
+    f"{os.path.dirname(os.path.abspath(__file__))}/processed/embeddings.csv", index_col=0)
 df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
 
+
+@backoff.on_exception(backoff.expo, openai.error.RateLimitError)
 def answer_question(
     question,
     df=df,
@@ -22,10 +23,10 @@ def answer_question(
     debug=False,
 ):
     """
-    Answer a question based on the most similar context from the dataframe texts
+    Answer a question based on the most similar context from the dataframe texts if no similar context is found create empty context
     """
     context = create_context(
-        question + " Also share me the link that will give me more information",
+        question ,
         df,
         # max_len=max_len,
         # size=size,
@@ -37,20 +38,19 @@ def answer_question(
         print("\n\n")
 
     try:
-        response = "This is not related to Fetch.ai" 
-        print("***************************************************************************************")
-        print(context)
+        response = "This is not related to Fetch.ai"
         if len(context) > 0:
             response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            temperature=0.5,
-            max_tokens=200,
-            messages=[
-                  {"role": "system", "content": f"You are a helpful assistant.fetch.ai is all called fetch in shorthand. You must strictly answer the question based on the context below, and if the question can't be answered based on the context or is not related to the context below, only say \"This is not related to Fetch.ai\".\n\nContext: {context}"},
-                  {"role": "user", "content": "Question: What is fetch.ai"},
-                  {"role": "assistant", "content": "Our mission is to build the infrastructure required for developing modern, decentralized and peer-to-peer (P2P) applications that are free from centralized rent-seeking."},
-                  {"role": "user", "content": f"Question: {question}?Also share me the link that will give me more information"}
-              ]
+                model="gpt-3.5-turbo",
+                temperature=0.5,
+                max_tokens=max_len,
+                messages=[
+                    {"role": "system", "content": f"You are a helpful assistant.fetch.ai is also called fetch in shorthand. You must strictly answer the question based on the context below, and if the question can't be answered based on the context or is not related to the context below, only say \"This is not related to Fetch.ai\".\n\nContext: {context}"},
+                    {"role": "user", "content": "Question: What is fetch.ai"},
+                    {"role": "assistant",
+                     "content": "Our mission is to build the infrastructure required for developing modern, decentralized and peer-to-peer (P2P) applications that are free from centralized rent-seeking."},
+                    {"role": "user", "content": f"Question: {question}? Also share me the link that will give me more information"}
+                ]
             )
             return response["choices"][0]["message"]["content"]
         print("resp", response)
@@ -58,7 +58,8 @@ def answer_question(
     except Exception as e:
         print(e)
         return ""
-    
+
+
 def get_embedding(text: str, model: str = EMBEDDING_MODEL):
     result = openai.Embedding.create(
         model=model,
@@ -67,21 +68,11 @@ def get_embedding(text: str, model: str = EMBEDDING_MODEL):
     return result["data"][0]["embedding"]
 
 
-def compute_doc_embeddings(df: pd.DataFrame):
-    """
-    Create an embedding for each row in the dataframe using the OpenAI Embeddings API.
-
-    Return a dictionary that maps between each embedding vector and the index of the row that it corresponds to.
-    """
-    return {
-        idx: get_embedding(r.text) for idx, r in df.iterrows()
-    }
-
 def create_context(
     question, df, max_len=500, size="ada"
 ):
     """
-    Create a context for a question by finding the most similar context from the dataframe
+    Create a context for a question by finding the most similar context from the dataframe if no similar context is found create empty context
     """
 
     # Get the embeddings for the question
@@ -92,7 +83,6 @@ def create_context(
         q_embeddings, df['embeddings'].values, distance_metric='cosine')
     returns = []
     cur_len = 0
-
     # Sort by distance and add the text to the context until the context is too long
     for i, row in df.sort_values('distances', ascending=True).iterrows():
         # Add the length of the text to the current length
@@ -109,6 +99,20 @@ def create_context(
     return ". ".join(returns)
 
 
+def moderationCheck(text):
+    response = openai.Moderation.create(
+        input=text
+    )
+    categories = response["results"][0].categories
+    tags = []
+    for category, value in categories.items():
+        if value:
+            tags.append(category)
+    if len(tags) != 0:
+        return("Your question has breached our moderation polices and has been classified with the following tags:" + ", ".join(tags)+". Please ask other questions")
+    return None
+
+
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -117,10 +121,11 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def index():
     if request.method == "POST":
         question = request.form["question"]
-        # print("question =>",question)
-        return redirect(url_for("index", result=answer_question(question, debug=False)))
+        moderation_result = moderationCheck(question)
+        if moderation_result == None:
+            return redirect(url_for("index", result=answer_question(question, debug=False)))
+        else:
+            return redirect(url_for("index", result=moderation_result))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
-
-
